97a8a81cf30eb08f400680da9b15eb6ebcede12d
duydt <duydt@vng.com.vn>
2023-02-23 20:31:40 +0700

    initialize

---
+++ src/main/java/com/onix/demospringsecurity/DemoSpringSecurityApplication.java
@@ -0,0 +1,16 @@
+package com.onix.demospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSecurityApplication.class, args);
+	}
+}
